a3f8ee7c9f6cc95087c902c5cf235e755ddccadc
conglt <thanhcong1710@gmail.com>
2024-02-02 21:30:39 +0700

    update

--- resources/js/themeConfig.js
+++ resources/js/themeConfig.js
@@ -18,7 +18,7 @@ export const colors = {
 
 // CONFIGS
 const themeConfig = {
-  disableCustomizer : false,       // options[Boolean] : true, false(default)
+  disableCustomizer : true,       // options[Boolean] : true, false(default)
   disableThemeTour  : true,        // options[Boolean] : true, false(default)
   footerType        : 'static',    // options[String]  : static(default) / sticky / hidden
   hideScrollToTop   : false,       // options[Boolean] : true, false(default)
